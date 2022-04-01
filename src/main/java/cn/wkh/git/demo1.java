b4cfafa400f23c10a74c8c73ff42023469cf1466
xinyuanyi <xinyuanyi@xiaoxin.cn>
2022-04-01 11:01:14 +0800

    hot-fix commit1

--- src/main/java/cn/wkh/git/demo1.java
+++ src/main/java/cn/wkh/git/demo1.java
@@ -3,5 +3,7 @@ package cn.wkh.git;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
